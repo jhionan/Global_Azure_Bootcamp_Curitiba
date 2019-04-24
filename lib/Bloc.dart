a7bd93d12c43dd6135957b429a7bd89e105cd4b9
Rian <jhionan@gmail.com>
2019-04-23 22:54:42 -0300

    feature (bloc) create

---
+++ lib/Bloc.dart
@@ -0,0 +1,23 @@
+import 'package:rxdart/rxdart.dart';
+
+class Bloc {
+  int _pageNumber = 0;
+
+  final _pages = PublishSubject<String>();
+
+  Observable<String> get page => _pages.stream;
+
+  void dispose() {
+    _pages.close();
+  }
+
+  void next() {
+    _pageNumber++;
+    _pages.add("$_pageNumber");
+  }
+
+  void preview() {
+    _pageNumber--;
+    _pages.add("$_pageNumber");
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,29 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      initialRoute: "/",
+      routes: <String, WidgetBuilder>{
+        "/" : (context) => _Home()
+      },
+    );
+  }
+}
+
+class _Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<_Home> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container()
+    );
   }
 }
