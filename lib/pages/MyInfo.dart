8099fdad2344329d4cdefd7952623366ae27893a
Rian <jhionan@gmail.com>
2019-04-24 22:15:55 -0300

    MyInfo

--- lib/pages/MyInfo.dart
+++ lib/pages/MyInfo.dart
@@ -8,68 +8,76 @@ class MyInfo extends StatefulWidget {
 class _MyInfoState extends State<MyInfo> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(80.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+    return Stack(
+      children: <Widget>[
+        Image.asset("assets/info_background.jpg",
+        width: double.infinity,
+        fit: BoxFit.fill,),
+        Padding(
+          padding: const EdgeInsets.all(80.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
             children: <Widget>[
-              Text("Jhionan Rian Lara dos Santos\nDesenvolvedor Mobile",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.w400
-              ),),
-            ],
-          ),
-          SizedBox(
-            height: 150,
-          ),
-          Row(
-            children: <Widget>[
-              Image.asset(
-                "assets/linkedin.png",
-                height: 110,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Jhionan Rian Lara dos Santos\nDesenvolvedor Mobile",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 80,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w400
+                  ),),
+                ],
               ),
               SizedBox(
-                width: 30,
+                height: 100,
               ),
-              Text("https://www.linkedin.com/in/jhionan"),
-            ],
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          Row(
-            children: <Widget>[
-              Image.asset(
-                "assets/github.png",
-                height: 120,
-              ),
-              SizedBox(
-                width: 30,
-              ),
-              Text("https://github.com/jhionan"),
-            ],
-          ),
-          SizedBox(height: 40,),
-          Row(
-            children: <Widget>[
-              Image.asset(
-                "assets/email.png",
-                height: 120,
+              Row(
+                children: <Widget>[
+                  Image.asset(
+                    "assets/linkedin.png",
+                    height: 110,
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Text("https://www.linkedin.com/in/jhionan"),
+                ],
               ),
               SizedBox(
-                width: 30,
+                height: 40,
               ),
-              Text("jhionan@gmail.com"),
+              Row(
+                children: <Widget>[
+                  Image.asset(
+                    "assets/github.png",
+                    height: 120,
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Text("https://github.com/jhionan"),
+                ],
+              ),
+              SizedBox(height: 40,),
+              Row(
+                children: <Widget>[
+                  Image.asset(
+                    "assets/email.png",
+                    height: 120,
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Text("jhionan@gmail.com"),
+                ],
+              ),
             ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
